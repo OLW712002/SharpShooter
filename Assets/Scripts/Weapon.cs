f4adfee6d414612ac6326bdf01a9a2833b816f5a
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-10-24 21:39:25 +0700

    Intro Raycast

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    
+    void Update()
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
+        if (hit.collider.name != null) Debug.Log(hit.collider.name);
+    }
+}
