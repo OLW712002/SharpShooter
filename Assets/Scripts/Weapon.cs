cfc1a0a5a68d14ac41aa6a02cad9d86a51e766db
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-10-28 15:53:22 +0700

    Update Weapon.cs
    Add fireCooldown

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using System.Collections;
 using StarterAssets;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] int gunDamage = 1;
+    [SerializeField] float fireCooldown = 1f;
 
     StarterAssetsInputs starterAssetsInpouts;
+
+    bool isOverHeat = false;
 
     void Awake()
     {
@@ -14,19 +18,30 @@ public class Weapon : MonoBehaviour
     
     void Update()
     {
-        if (starterAssetsInpouts.shoot)
+        if (starterAssetsInpouts.shoot && !isOverHeat)
         {
-            RaycastHit hit;
-
-            Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
-            if (hit.collider != null)
-            {
-                hit.collider.GetComponent<Robot>()?.TakeDamage(gunDamage);
-                Debug.Log(hit.collider.name);
-            }
-            starterAssetsInpouts.ShootInput(false);
+            ShootProcess();
         }
+    }
 
-        
+    void ShootProcess()
+    {
+        isOverHeat = true;
+        StartCoroutine(OverHeatCoroutine(fireCooldown));
+
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponent<Robot>()?.TakeDamage(gunDamage);
+            Debug.Log(hit.collider.name);
+        }
+        starterAssetsInpouts.ShootInput(false);
+    }
+
+    IEnumerator OverHeatCoroutine(float cooldown)
+    {
+        yield return new WaitForSecondsRealtime(cooldown);
+        isOverHeat = false;
     }
 }
