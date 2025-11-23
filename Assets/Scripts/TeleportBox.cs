bb9475d59a181b9ad99f51d818592506745908d8
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-11-23 19:38:06 +0700

    Add: TeleportBox prototype
    Problem: Can't teleport player

---
+++ Assets/Scripts/TeleportBox.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using StarterAssets;
+
+public class TeleportBox : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FirstPersonController playerController = other.GetComponent<FirstPersonController>();
+            BasicRigidBodyPush basicRigidBodyPush = other.GetComponent<BasicRigidBodyPush>();
+            StarterAssetsInputs starterAssetsInputs = other.GetComponent<StarterAssetsInputs>();
+
+            playerController.enabled = false;
+            basicRigidBodyPush.enabled = false;
+            starterAssetsInputs.enabled = false;
+
+            Debug.Log("box " + other.gameObject.name);
+            other.transform.position = target.position;
+            
+            playerController.enabled = true;
+            basicRigidBodyPush.enabled = true;
+            basicRigidBodyPush.enabled = true;
+        }
+    }
+}
