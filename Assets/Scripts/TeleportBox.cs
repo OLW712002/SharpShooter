317f45e611bb3b37471b8c18ac66e4dac9753c5b
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-11-24 16:34:12 +0700

    Complete TeleportBox

--- Assets/Scripts/TeleportBox.cs
+++ Assets/Scripts/TeleportBox.cs
@@ -9,20 +9,23 @@ public class TeleportBox : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            FirstPersonController playerController = other.GetComponent<FirstPersonController>();
-            BasicRigidBodyPush basicRigidBodyPush = other.GetComponent<BasicRigidBodyPush>();
-            StarterAssetsInputs starterAssetsInputs = other.GetComponent<StarterAssetsInputs>();
+            //FirstPersonController playerController = other.GetComponent<FirstPersonController>();
+            //BasicRigidBodyPush basicRigidBodyPush = other.GetComponent<BasicRigidBodyPush>();
+            //StarterAssetsInputs starterAssetsInputs = other.GetComponent<StarterAssetsInputs>();
+            CharacterController characterController = other.GetComponent<CharacterController>();
 
-            playerController.enabled = false;
-            basicRigidBodyPush.enabled = false;
-            starterAssetsInputs.enabled = false;
+            //playerController.enabled = false;
+            //basicRigidBodyPush.enabled = false;
+            //starterAssetsInputs.enabled = false;
+            characterController.enabled = false;
 
             Debug.Log("box " + other.gameObject.name);
             other.transform.position = target.position;
-            
-            playerController.enabled = true;
-            basicRigidBodyPush.enabled = true;
-            basicRigidBodyPush.enabled = true;
+
+            //playerController.enabled = true;
+            //basicRigidBodyPush.enabled = true;
+            //basicRigidBodyPush.enabled = true;
+            characterController.enabled = true;
         }
     }
 }
