66e62ea1f1916e79ced2990f103fc281635fc326
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-11-27 13:49:49 +0700

    Bring all function to activeweapon

--- Assets/Scripts/ActiveWeapon.cs
+++ Assets/Scripts/ActiveWeapon.cs
@@ -1,16 +1,65 @@
+using StarterAssets;
 using UnityEngine;
+using System.Collections;
 
-public class ActiveWeapon : MonoBehaviour
+public class ActiveWeapon : Weapon
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] ParticleSystem gunFlash;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] Transform vfxParent;
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] WeaponSO gunType;
+    //[SerializeField] int gunDamage = 1;
+    //[SerializeField] float fireCooldown = 1f;
+
+    StarterAssetsInputs starterAssetsInpouts;
+
+    bool isOverHeat = false;
+    bool wasShooting = false;
+
+    const string playerShootString = "Shoot";
+
+    void Awake()
     {
-        
+        starterAssetsInpouts = GetComponentInParent<StarterAssetsInputs>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P)) Debug.Log(starterAssetsInpouts.shoot);
+
+        if (starterAssetsInpouts.shoot && !isOverHeat && !wasShooting)
+        {
+            ShootProcess();
+        }
+        wasShooting = starterAssetsInpouts.shoot;
+        starterAssetsInpouts.ShootInput(false);
+    }
+
+    void ShootProcess()
+    {
+        isOverHeat = true;
+        StartCoroutine(OverHeatCoroutine(gunType.fireCooldown));
+
+        gunFlash.Play();
+        playerAnimator.Play(playerShootString, 0, 0);
+
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
+        if (hit.collider != null)
+        {
+            hit.collider.GetComponentInParent<Robot>()?.TakeDamage(gunType.gunDmg);
+            Debug.Log(hit.collider.name);
+        }
+        if (hit.point != null)
+        {
+            Destroy(Instantiate(hitVFX, hit.point, Quaternion.identity, vfxParent), 5f);
+        }
+    }
+
+    IEnumerator OverHeatCoroutine(float cooldown)
+    {
+        yield return new WaitForSecondsRealtime(cooldown);
+        isOverHeat = false;
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,64 +2,7 @@ using UnityEngine;
 using System.Collections;
 using StarterAssets;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
-    [SerializeField] ParticleSystem gunFlash;
-    [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform vfxParent;
-    [SerializeField] Animator playerAnimator;
-    [SerializeField] WeaponSO gunType;
-    //[SerializeField] int gunDamage = 1;
-    //[SerializeField] float fireCooldown = 1f;
 
-    StarterAssetsInputs starterAssetsInpouts;
-
-    bool isOverHeat = false;
-    bool wasShooting = false;
-
-    const string playerShootString = "Shoot";
-
-    void Awake()
-    {
-        starterAssetsInpouts = GetComponentInParent<StarterAssetsInputs>();
-    }
-    
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.P)) Debug.Log(starterAssetsInpouts.shoot);
-
-        if (starterAssetsInpouts.shoot && !isOverHeat && !wasShooting)
-        {
-            ShootProcess();
-        }
-        wasShooting = starterAssetsInpouts.shoot;
-        starterAssetsInpouts.ShootInput(false);
-    }
-
-    void ShootProcess()
-    {
-        isOverHeat = true;
-        StartCoroutine(OverHeatCoroutine(gunType.fireCooldown));
-
-        gunFlash.Play();
-        playerAnimator.Play(playerShootString, 0, 0);
-
-        RaycastHit hit;
-        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
-        if (hit.collider != null)
-        {
-            hit.collider.GetComponentInParent<Robot>()?.TakeDamage(gunType.gunDmg);
-            Debug.Log(hit.collider.name);
-        }
-        if (hit.point != null)
-        {
-            Destroy(Instantiate(hitVFX, hit.point, Quaternion.identity, vfxParent), 5f);
-        }
-    }
-
-    IEnumerator OverHeatCoroutine(float cooldown)
-    {
-        yield return new WaitForSecondsRealtime(cooldown);
-        isOverHeat = false;
-    }
 }
