dfc4a7b6fc968adc42556b7444f3f01a7f55bfec
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-12-02 17:59:12 +0700

    Complete: weapon.cs is abstract class

--- Assets/Scripts/ActiveWeapon.cs
+++ Assets/Scripts/ActiveWeapon.cs
@@ -1,21 +1,12 @@
 using StarterAssets;
 using UnityEngine;
-using System.Collections;
 
 public class ActiveWeapon : Weapon
 {
-    [SerializeField] Transform vfxParent;
-    [SerializeField] Transform gunFlashParent;
-    [SerializeField] Animator playerAnimator;
     [SerializeField] WeaponSO gunType;
 
     StarterAssetsInputs starterAssetsInpouts;
-
-    bool isOverHeat = false;
-    bool wasShooting = false;
-
-    const string playerShootString = "Shoot";
-
+    
     void Awake()
     {
         starterAssetsInpouts = GetComponentInParent<StarterAssetsInputs>();
@@ -33,32 +24,5 @@ public class ActiveWeapon : Weapon
         starterAssetsInpouts.ShootInput(false);
     }
 
-    void ShootProcess(WeaponSO weapon)
-    {
-        isOverHeat = true;
-        StartCoroutine(OverHeatCoroutine(weapon.fireCooldown));
-
-        ParticleSystem gunFlashParticle = Instantiate(weapon.gunFlash, gunFlashParent.position, gunFlashParent.rotation, gunFlashParent);
-        Destroy(gunFlashParticle.gameObject, 2f);
-
-        playerAnimator.Play(playerShootString, 0, 0);
-
-        RaycastHit hit;
-        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
-        if (hit.collider != null)
-        {
-            hit.collider.GetComponentInParent<Robot>()?.TakeDamage(weapon.gunDmg);
-            Debug.Log(hit.collider.name);
-        }
-        if (hit.point != null)
-        {
-            Destroy(Instantiate(weapon.hitVFX, hit.point, Quaternion.identity, vfxParent), 5f);
-        }
-    }
-
-    IEnumerator OverHeatCoroutine(float cooldown)
-    {
-        yield return new WaitForSecondsRealtime(cooldown);
-        isOverHeat = false;
-    }
+    
 }
