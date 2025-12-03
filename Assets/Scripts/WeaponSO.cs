f3e0b90b227c2e5f2e92e6a570195c2d2c80d085
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-12-03 18:46:25 +0700

    Fix: Conflict when merge branch

--- Assets/Scripts/WeaponSO.cs
+++ Assets/Scripts/WeaponSO.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/WeaponSO")]
 public class WeaponSO : ScriptableObject
 {
+    public int gunDmg = 1;
+    public float fireCooldown = 1f;
     public ParticleSystem gunFlash;
     public GameObject hitVFX;
+    
 }
