00647e1eaf6c707f2b2ecb858da35ffac389614a
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-12-25 21:52:09 +0700

    Reorganize scripts

---
+++ Assets/Scripts/Weapon/Pickups/Pickups.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public abstract class Pickups : Weapon
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
