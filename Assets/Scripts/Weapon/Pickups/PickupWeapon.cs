613bdafb2eb4fd0a35d13eb43e2bb50d3d1db8bb
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-12-27 17:55:10 +0700

    Refactor pickup weapon

--- Assets/Scripts/Weapon/Pickups/PickupWeapon.cs
+++ Assets/Scripts/Weapon/Pickups/PickupWeapon.cs
@@ -4,29 +4,9 @@ public class PickupWeapon : Pickups
 {
     const string currentWeaponString = "Current Weapon";
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void OnPickup(WeaponSO weaponSO)
     {
-        //Transform currentWeapon = GameObject.Find(currentWeaponString).transform;
-    }
-
-    // Update is called once per frame
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag(playerTagString))
-        {
-            OnPickup();
-        }
-    }
-
-    protected override void OnPickup()
-    {
-        //other.gameObject.GetComponentInChildren<ActiveWeapon>().SwitchWeapon(weaponSO);
         Destroy(FindFirstObjectByType<ActiveWeapon>().gameObject);
         Instantiate(weaponSO.weaponPrefab, GameObject.Find(currentWeaponString).transform);
-        //this.enabled = false;
-        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Weapon/Pickups/Pickups.cs
+++ Assets/Scripts/Weapon/Pickups/Pickups.cs
@@ -4,16 +4,19 @@ public abstract class Pickups : Weapon
 {
     [SerializeField] protected float rotateSpeed = 2f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
     }
 
-    protected abstract void OnPickup();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTagString))
+        {
+            OnPickup(weaponSO);
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract void OnPickup(WeaponSO weaponSO);
 }
