3557842e6ff8588f1cbc49a47e5da7fdc435a980
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2025-12-29 16:44:07 +0700

    Add PickupAmmo
    Prepare before implement

---
+++ Assets/Scripts/Weapon/Pickups/PickupAmmo.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PickupAmmo : Pickups
+{
+    protected override void OnPickup(WeaponSO weaponSO)
+    {
+        throw new System.NotImplementedException();
+    }
+}
