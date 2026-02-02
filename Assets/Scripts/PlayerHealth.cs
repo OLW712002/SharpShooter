d2508aa1a03085e011903949724fe15a029dca58
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2026-02-02 18:36:31 +0700

    Init PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] int playerHealth = 5;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int dmg)
     {
-        
+        playerHealth -= dmg;
+        if (playerHealth <= 0)
+        {
+            Debug.Log("PLayer Die");
+        }
     }
 }
