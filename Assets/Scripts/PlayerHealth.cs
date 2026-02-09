3c8a5f66b7dd463e71da67ebbb60a318c6242306
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2026-02-09 17:04:29 +0700

    Update Shield (Health) UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,28 +1,45 @@
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int playerHealth = 5;
+    [Range(1,10)] [SerializeField] int playerHealth = 5;
     [SerializeField] CinemachineVirtualCamera playerDeathCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
 
     int gameoverVirtualCameraPriority = 20;
 
     void Start()
     {
-
+        UpdateShieldBars();
     }
 
     public void TakeDamage(int dmg)
     {
-        playerHealth -= dmg;
+        //playerHealth -= dmg;
+        playerHealth = Mathf.Clamp(playerHealth -= dmg, 0, int.MaxValue);
+        UpdateShieldBars();
+
         if (playerHealth <= 0)
         {
             weaponCamera.parent = null;
             playerDeathCamera.Priority = gameoverVirtualCameraPriority;
             Debug.Log("PLayer Die");
             Destroy(this.gameObject);
+        }
+    }
+
+    void UpdateShieldBars()
+    {
+        for (int i = 0; i < playerHealth; i++)
+        {
+            shieldBars[i].color = Color.white;
+        }
+        for (int i = playerHealth; i < shieldBars.Length; i++)
+        {
+            shieldBars[i].color = Color.black;
         }
     }
 }
