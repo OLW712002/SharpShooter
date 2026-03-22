6619ed750b1b40b288a86c2f8d538a28c706b438
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2026-03-22 19:53:51 +0700

    Clean code
    Change return value of InstantExplosion.GetSelfDestructDelay()

--- Assets/Scripts/Explosion/ExplosionBehavior/InstantExplosion.cs
+++ Assets/Scripts/Explosion/ExplosionBehavior/InstantExplosion.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 [System.Serializable]
 public class InstantExplosion : ExplosionBehavior
 {
-    //DestroyType destroyType = DestroyType.Instant;
-    float enemySelfDestructDelay = 0f;
-
     public override IEnumerator BehaviorBeforeExploding(Transform ownerTransform)
     {
-        //ExplosionParameters explosionParameters = new ExplosionParameters(destroyType, enemyExplosion);
-        //No pre action before explosion for instant explosion
         yield return null;
     }
 
     public override float GetSelfDestructDelay()
     {
-        return enemySelfDestructDelay;
+        return 0f;
     }
 }
--- Assets/Scripts/Explosion/ExplosionBehavior/ShakeUnstableExplosion.cs
+++ Assets/Scripts/Explosion/ExplosionBehavior/ShakeUnstableExplosion.cs
@@ -4,30 +4,12 @@ using UnityEngine;
 [System.Serializable]
 public class ShakeUnstableExplosion : ExplosionBehavior
 {
-    //DestroyType destroyType = DestroyType.ShakeUnstable;
-
     [SerializeField] float enemyShakeDuration = 1f;
     [SerializeField] float enemyMaxShakeMagnitude = 10f;
     [SerializeField] AnimationCurve enemyMagnitudeOverTime = AnimationCurve.Constant(0f, 1f, 0f);
 
     public override IEnumerator BehaviorBeforeExploding(Transform ownerTransform)
     {
-        //ExplosionParameters explosionParameters = new ExplosionParameters(destroyType, enemyExplosion, enemyShakeDuration, enemyMaxShakeMagnitude, enemyMagnitudeOverTime);
-
-        //Vector3 originalLocalRotate = ownerTransform.localEulerAngles;
-        //float elapsedTimeShake = 0f;
-        //float internalFreq = 20f;
-        //while (elapsedTimeShake < explosionParameters.ShakeDuration)
-        //{
-        //    elapsedTimeShake += Time.deltaTime;
-        //    float normalizedTime = elapsedTimeShake / explosionParameters.ShakeDuration;
-        //    float currentMagnitude = explosionParameters.MaxShakeMagnitude * explosionParameters.MagnitudeOverTime.Evaluate(normalizedTime);
-        //    float offsetZ = Mathf.Sin(elapsedTimeShake * internalFreq * Mathf.PI * 2f) * currentMagnitude;
-        //    ownerTransform.localEulerAngles = originalLocalRotate + new Vector3(0f, 0f, offsetZ);
-        //    yield return null;
-        //}
-        //ownerTransform.localPosition = originalLocalRotate;
-
         Vector3 originalLocalRotate = ownerTransform.localEulerAngles;
         float elapsedTimeShake = 0f;
         float internalFreq = 20f;
