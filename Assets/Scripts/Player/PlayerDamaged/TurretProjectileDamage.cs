1d5406c1a3d3b5e80a0fadebbb87236f8cfd55cc
NguyenVanDuy20207666 <duytp8c1@gmail.com>
2026-04-03 05:31:13 +0700

    Add another logic to detect player, Add projectile interact with pickup

--- Assets/Scripts/Player/PlayerDamaged/TurretProjectileDamage.cs
+++ Assets/Scripts/Player/PlayerDamaged/TurretProjectileDamage.cs
@@ -29,15 +29,21 @@ public class TurretProjectileDamage : PlayerDamaged
         Debug.Log(transform.position);
         if (other.CompareTag("Player"))
         {
-            //Use ClosestPoint and OverlapSphere
+            //Use ClosestPoint and OverlapSphere for detection
             transform.position = other.ClosestPoint(transform.position);
             Debug.Log("Turret projectile hit player: " + other.name);
             int playerLayerMask = LayerMask.GetMask(playerLayerString);
             Collider[] hitCollider = Physics.OverlapSphere(transform.position, 0.1f, playerLayerMask, QueryTriggerInteraction.Ignore);
-            
-            
             HitPlayer(hitCollider, turretDmg);
+
+            //Normal detection
+            //other.GetComponentInParent<PlayerHealth>()?.TakeDamage(turretDmg);
+
             Destroy(gameObject);
+        }
+        else if (other.CompareTag("Pickup"))
+        {
+            Debug.Log("Turret projectile hit pickup: " + other.name);
         }
         else
         {
